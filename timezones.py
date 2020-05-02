dac9a7d30e3d419b398d7b1d9141fd654c86f347
ApthNZ <24648796+ApthNZ@users.noreply.github.com>
2020-05-03 10:24:38 +1200

    who knows

---
+++ timezones.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+Script that takes a date and time as an input,
+and returns that date and time in some other
+timezones.
+"""
+
+
+#TODO: Write tests first! Revolutionary.
+
+
+# Take a time input
+
+
+# Figure out which other timezones to convert it to
+
+
+# Do that conversion
+
+
+# Return the timezones nicely formatted
+
+
+#TODO: Option (flag?) to automatically copy to clipboard
+#TODO: A config file that stores the local TZ and destination TZs
